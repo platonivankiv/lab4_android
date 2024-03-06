9a1acf306e41ad815632056b6602e0a2448b8e84
platon.ivankiv <ivankiv.platon@gmail.com>
2024-03-06 14:18:00 +0300

    second commit

--- app/src/main/java/by/grsu/guess_the_number/GuessNum.java
+++ app/src/main/java/by/grsu/guess_the_number/GuessNum.java
@@ -3,7 +3,7 @@ package by.grsu.guess_the_number;
 import java.util.Random;
 
 public class GuessNum {
-    static public int rndCompNum0(){
+    static public int rndCompNum(){
         int min = 10;
         int max = 99;
         int diff = max - min;
--- app/src/main/java/by/grsu/guess_the_number/MainActivity.java
+++ app/src/main/java/by/grsu/guess_the_number/MainActivity.java
@@ -1,9 +1,8 @@
-package by.grsu.guess_the_number;
+ package by.grsu.guess_the_number;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,9 +15,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView showMsgTextView;
     private TextView showHintTextView;
     private EditText editNumEditText;
-    private Button btnGuess;
     private TextView attemptsLeftTextView;
     private Button btnRestart;
+    private Button btnGuess;
+
+    private TextView compNumTextView;
     int comp_num = 0;
 
     @Override
@@ -27,63 +28,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        comp_num = GuessNum.rndCompNum();
+        btnGuess = findViewById(R.id.btn_guess);
+
+        editNumEditText = findViewById(R.id.edit_num);
         showMsgTextView = findViewById(R.id.show_msg);
         showHintTextView = findViewById(R.id.show_hint);
         attemptsLeftTextView = findViewById(R.id.attempts_left);
+        btnRestart = findViewById(R.id.btn_restart);
 
-        // Установка размера и цвета шрифта для showMsgTextView
+
+        // Установка размера и цвета шрифта
         showMsgTextView.setTextSize(24);
         showMsgTextView.setTextColor(getResources().getColor(R.color.red));
 
-        // Установка размера и цвета шрифта для showHintTextView
         showHintTextView.setTextSize(18);
         showHintTextView.setTextColor(getResources().getColor(R.color.blue));
 
-        // Установка размера и цвета шрифта для attemptsLeftTextView
         attemptsLeftTextView.setTextSize(20);
         attemptsLeftTextView.setTextColor(getResources().getColor(R.color.green));
 
-
-        editNumEditText = findViewById(R.id.edit_num);
-        btnGuess = findViewById(R.id.btn_guess);
-        btnRestart = findViewById(R.id.btn_restart);
-
-        // Установка текста из строковых ресурсов
-        showMsgTextView.setText(R.string.show_msg_label);
-        showHintTextView.setText(R.string.show_hint_label);
-        editNumEditText.setHint(R.string.edit_hint_label);
-        btnGuess.setText(R.string.btn_guess_label);
-        attemptsLeftTextView.setText(getString(R.string.attempts_left_label, 5));
-        btnRestart.setText(R.string.btn_restart_label);
-
-        comp_num = GuessNum.rndCompNum0();
-        TextView compNumTextView = findViewById(R.id.comp_num_label);
+        // Загаданное число
+        compNumTextView = findViewById(R.id.comp_num_label);
         compNumTextView.setText(String.valueOf(comp_num));
 
-//todo: написать restart
-//        private void restart () {
-//            showMsgTextView.setText(getString(R.string.show_msg_label));
-//            showHintTextView.setText(getString(R.string.show_hint_label));
-//            attemptsLeftTextView.setText(getString(R.string.attempts_left_label));
-//        }
 
         View.OnClickListener btnGuessListener = new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                String guessedNumberStr = editNumEditText.getText().toString(); // Получаем введенное пользователем число из EditText
-
+            public void onClick(View view) {
                 try {
-                    int guessedNumber = Integer.parseInt(guessedNumberStr); // Преобразуем строку в число
+                    int user_num = Integer.parseInt(editNumEditText.getText().toString());
 
-                    if (guessedNumber == comp_num) {
-                        // Угадано
-                        btnGuess.setText(getString(R.string.guessed_button_label));
-                        btnGuess.setEnabled(false);
-                        Toast.makeText(getApplicationContext(), getString(R.string.congrats_message), Toast.LENGTH_SHORT).show();
+                    if (user_num == comp_num) {
+                        btnGuess.setText(R.string.guessed_msg_label);
+                        view.setEnabled(false);
+                        Toast.makeText(getApplicationContext(), getString(R.string.congrats_msg_label), Toast.LENGTH_SHORT).show();
                     } else {
-                        // Не угадано
-                        Toast.makeText(getApplicationContext(), getString(R.string.try_again_message), Toast.LENGTH_SHORT).show();
+
+                        int left = Integer.parseInt(attemptsLeftTextView.getText().toString());
+
+                        if (user_num > comp_num) {
+                            showHintTextView.setText(R.string.show_user_number_greater);
+
+                        } else {
+                            showHintTextView.setText(R.string.show_user_number_less);
+                        }
+                        left--;
+
+                        if (left == 0) {
+                            showMsgTextView.setText(R.string.restart_msg_label);
+                            view.setEnabled(false);
+                            Toast.makeText(getApplicationContext(), getString(R.string.game_over_msg_label), Toast.LENGTH_SHORT).show();
+                        }
+
+                        attemptsLeftTextView.setText(String.valueOf(left));
+                        editNumEditText.setText("");
                     }
                 } catch (NumberFormatException e) {
                     // Некорректный формат числа
@@ -93,7 +92,15 @@ public class MainActivity extends AppCompatActivity {
         };
         btnGuess.setOnClickListener(btnGuessListener);
     }
+    public void restart(View view) {
+        comp_num = GuessNum.rndCompNum();
+        compNumTextView.setText(String.valueOf(comp_num));
+        btnGuess.setEnabled(true);
+        btnGuess.setText(R.string.btn_guess_label);
+        editNumEditText.setText("");
+        showMsgTextView.setText(getString(R.string.show_msg_label));
+        showHintTextView.setText(getString(R.string.show_hint_label));
+        attemptsLeftTextView.setText(R.string.attempts_left_label);
 
-
+    }
 }
-
